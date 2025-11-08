854f41188a21f6e906293732db0bb3598b049512
laraa-n <nicolelara.sa@gmail.com>
2025-11-08 19:17:54 -0300

    feat: adicionando rota de post

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const banco = require('./banco');
 const app = express();
 
 const PORT = 3000;
@@ -8,6 +8,24 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API rodando!');
+});
+
+app.post('/tasks', (req, res) => {
+  try {
+    const { title, description } = req.body;
+
+    if (!title) {
+      return res.status(400).json({ error: 'O campo "title" é obrigatório.' });
+    }
+
+    const newTask = banco.createTask(title, description);
+
+    res.status(201).json(newTask);
+
+  } catch (error) {
+    res.status(500).json({ error: 'Erro interno ao criar a tarefa.' });
+  }
+
 });
 
 app.listen(PORT, () => {
